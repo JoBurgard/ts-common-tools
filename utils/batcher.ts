65b7f663d97eb8a2fd614bfe943cc4d787b801b8
Jo Burgard <mail@joburgard.com>
2025-12-01 16:27:45 +0100

    feat: batch system

---
+++ utils/batcher.ts
@@ -0,0 +1,45 @@
+export function createBatchSystem<Context>() {
+	const batchList: Array<
+		[
+			fn: (context: Context) => void,
+			resolve: (val: unknown) => void,
+			reject: (reason?: any) => void,
+		]
+	> = [];
+
+	const add = <Return>(fn: (context: Context) => Return) => {
+		const prom = new Promise<Return>((resolve, reject) => {
+			batchList.push([fn, resolve as any, reject]);
+		});
+
+		return prom;
+	};
+
+	const run = (context: Context) => {
+		if (batchList.length < 1) {
+			return;
+		}
+
+		const list = structuredClone(batchList);
+		batchList.length = 0;
+
+		for (const [fn, resolve, reject] of list) {
+			try {
+				const result = fn(context);
+				resolve(result);
+			} catch (error) {
+				reject(error);
+			}
+		}
+	};
+
+	const isEmpty = () => {
+		return batchList.length === 0;
+	};
+
+	return {
+		add,
+		run,
+		isEmpty,
+	};
+}
