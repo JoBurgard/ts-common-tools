c6674ccb584a04c58bd08b2ef4fa8eef8d2bfeff
Jo Burgard <mail@joburgard.com>
2025-12-05 16:14:06 +0100

    feat: extend job system

--- job-system/index.ts
+++ job-system/index.ts
@@ -3,6 +3,8 @@ import { and, eq } from 'drizzle-orm';
 import { Result } from '../utils/result';
 import { job } from './schema/job';
 
+type JobStatus = 'init' | 'in progress' | 'fail' | 'success';
+
 export function createJobSystem<CreateParams extends Record<string, unknown>>(params: {
 	name: string;
 	version: number;
@@ -15,7 +17,7 @@ export function createJobSystem<CreateParams extends Record<string, unknown>>(pa
 						id: generateId(),
 						creator: creator,
 						taker: params.name,
-						status: 'created',
+						status: 'init' as JobStatus,
 						parameters: jobParams,
 						version: params.version,
 					})
@@ -34,18 +36,18 @@ export function createJobSystem<CreateParams extends Record<string, unknown>>(pa
 		});
 	};
 
-	const takeJob = async () => {
+	const take = async () => {
 		return await Result.try(async () => {
 			const [item] = await dbBatcher.add((tx) => {
 				return tx
 					.update(job)
 					.set({
-						status: 'taken',
+						status: 'in progress' as JobStatus,
 					})
 					.where(
 						and(
 							eq(job.taker, params.name),
-							eq(job.status, 'created'),
+							eq(job.status, 'init' as JobStatus),
 							eq(job.version, params.version),
 						),
 					)
@@ -54,13 +56,31 @@ export function createJobSystem<CreateParams extends Record<string, unknown>>(pa
 					.returning();
 			});
 
-			return item;
+			return item as unknown as
+				| (Omit<Exclude<typeof item, undefined>, 'parameters'> & {
+						parameters: CreateParams;
+				  })
+				| undefined;
+		});
+	};
+
+	const update = async (
+		id: string,
+		params: { status: JobStatus; context?: Record<string, unknown> },
+	) => {
+		return await Result.try(async () => {
+			await dbBatcher.add((tx) => {
+				tx.update(job)
+					.set({ status: params.status, statusContext: params.context ?? {} })
+					.where(eq(job.id, id));
+			});
 		});
 	};
 
 	return {
 		create,
 		remove,
-		takeJob,
+		take,
+		update,
 	};
 }
