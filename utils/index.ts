44cf78ff72128e20e6252969dabb4dcd647f90e9
Jo Burgard <mail@joburgard.com>
2026-01-05 15:03:02 +0100

    feat: add utils

--- utils/index.ts
+++ utils/index.ts
@@ -25,6 +25,26 @@ export function throttle<R, A extends any[]>(
 	};
 }
 
+export async function* interval(
+	ms: number,
+	options: { signal?: AbortSignal } = {},
+): AsyncGenerator<void, void, unknown> {
+	yield;
+
+	while (!options.signal?.aborted) {
+		await Bun.sleep(ms);
+		if (!options.signal?.aborted) {
+			yield;
+		}
+	}
+}
+
+export function waitForAbort(signal: AbortSignal) {
+	return new Promise((resolve) => {
+		signal.addEventListener('abort', () => resolve(undefined));
+	});
+}
+
 // tagged templates
 export const ts = String.raw;
 export const js = String.raw;
