1bec5639c48b103a5a242d774e6486798ca0e483
Jo Burgard <mail@joburgard.com>
2026-03-31 17:50:19 +0200

    fix: comment out vary header handling for now as it breaks the elysia static plugin

--- security/csrf-protection.ts
+++ security/csrf-protection.ts
@@ -6,34 +6,37 @@ import type Elysia from 'elysia';
  */
 export function csrfProtection() {
 	return (app: Elysia) =>
-		app
-			.onBeforeHandle(({ request, status }) => {
-				// INFO SAFE
-				// GET needs no protection, as long as all GET-endpoints are read only
-				if (request.method === 'GET') {
-					return;
-				}
+		app.onBeforeHandle(({ request, status }) => {
+			// INFO SAFE
+			// GET needs no protection, as long as all GET-endpoints are read only
+			if (request.method === 'GET') {
+				return;
+			}
 
-				const secFetchSite = request.headers.get('Sec-Fetch-Site');
+			const secFetchSite = request.headers.get('Sec-Fetch-Site');
 
-				// INFO SAFE
-				// the browser tells us, that the request comes from our site
-				if (secFetchSite === 'same-origin' || secFetchSite === 'same-site') {
-					return;
-				}
+			// INFO SAFE
+			// the browser tells us, that the request comes from our site
+			if (secFetchSite === 'same-origin') {
+				return;
+			}
 
-				// INFO NOT SAFE
-				// the request does not come from our site, we will not allow it
-				return status(403);
-			})
-			.onAfterHandle(({ set }) => {
-				// set Vary header to account for caching scenarios
-				const current = set.headers['Vary'];
+			// INFO NOT SAFE
+			// the request does not come from our site, we will not allow it
+			return status(403);
+		});
 
-				if (current && typeof current === 'string') {
-					set.headers['Vary'] = current + ', Sec-Fetch-Site';
-				} else {
-					set.headers['Vary'] = 'Sec-Fetch-Site';
-				}
-			});
+	// WARNING Unfortunately this breaks the elysia static plugin, so it is commented out for now
+	// it should not be a security concern
+
+	// .onAfterHandle(({ set }) => {
+	// 	// set Vary header to account for caching scenarios
+	// 	const current = set.headers['vary'];
+	//
+	// 	if (current && typeof current === 'string') {
+	// 		set.headers['vary'] = current + ', Sec-Fetch-Site';
+	// 	} else {
+	// 		set.headers['something'] = 'Sec-Fetch-Site';
+	// 	}
+	// });
 }
