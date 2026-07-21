c5cfa3933d7449fc05009e909fefafb22ad78390
Jo Burgard <mail@joburgard.com>
2026-07-21 16:19:19 +0200

    fix: double month

--- utils/date.ts
+++ utils/date.ts
@@ -1,7 +1,6 @@
 export function dateFormatEuropeanDateTime(date: Date) {
 	let res = String(date.getDate()).padStart(2, '0');
 	res += '.' + String(date.getMonth() + 1).padStart(2, '0');
-	res += '.' + String(date.getMonth() + 1).padStart(2, '0');
 	res += '.' + String(date.getFullYear()).slice(-2);
 	res += ', ' + String(date.getHours()).padStart(2, '0');
 	res += ':' + String(date.getMinutes()).padStart(2, '0');
