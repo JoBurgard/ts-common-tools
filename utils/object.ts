b3dc31bc32920a6727d1657bf603381b06ddab00
Jo Burgard <mail@joburgard.com>
2026-08-18 16:35:15 +0200

    feat: utils - objectInflate

--- utils/object.ts
+++ utils/object.ts
@@ -23,3 +23,24 @@ export function objectMergeDeep(
 
 	return objectMergeDeep(target, ...sources);
 }
+
+/**
+ * `{ 'a.b.c': 'bar' }` --> `{ a: { b: { c: 'bar } } }`
+ */
+export function objectInflate(input: Record<string, unknown>): Record<string, unknown> {
+	const output: Record<string, unknown> = {};
+	for (const key in input) {
+		const parts = key.split('.');
+		if (parts.length === 1) {
+			output[key] = input[key];
+		} else {
+			let curr: Record<string, unknown> = output;
+			for (const part of parts.slice(0, -1)) {
+				curr[part] ??= {};
+				curr = curr[part] as Record<string, unknown>;
+			}
+			curr[parts.at(-1)!] = input[key];
+		}
+	}
+	return output;
+}
